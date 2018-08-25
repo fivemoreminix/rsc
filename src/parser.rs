635b184197468cc2d400f88eafc519f55eb42b24
Luke Wilson <13669835+asmoaesl@users.noreply.github.com>
2018-08-24 19:29:22 -0500

    Little bug fix

--- src/parser.rs
+++ src/parser.rs
@@ -20,7 +20,7 @@ fn parse_additive_expr(tokens: &mut PeekableNth<Iter<Token>>) -> Expr {
         match tokens.peek_nth(0) {
             Some(Token::Operator(op)) if op == &Operator::Plus || op == &Operator::Minus => {
                 tokens.next();
-                let r_expr = parse_multiplicative_expr(tokens);
+                let r_expr = parse_additive_expr(tokens);
                 return Expr::BinOp(*op, Box::new(expr), Box::new(r_expr));
             }
             _ => break,
