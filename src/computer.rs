c89dcc70cf8046b2a2cd363f2bf7e629d8af8c64
Computer Science Classes <computerscienceclasses@Valley-View-HS-MacBook-Air-3.local>
2018-12-13 11:54:18 -0600

    Stuff

--- src/computer.rs
+++ src/computer.rs
@@ -4,12 +4,78 @@
 use crate::lexer::*;
 use crate::parser::*;
 
+use std::collections::HashMap;
+
 // If you come bearing big changes, you may have to rewrite
 // this to suit your needs.
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum ComputeError {
     UnrecognizedIdentifier(String),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Computer {
+    variables: HashMap<String, f64>,
+}
+
+impl Computer {
+    pub fn eval(&mut self, expr: &str) -> Result<f64, EvalError> {
+        match tokenize(expr) {
+            Ok(tokens) => match parse(&tokens) {
+                Ok(ast) => match self.compute(&ast) {
+                    Ok(num) => Ok(num),
+                    Err(compute_err) => Err(EvalError::ComputeError(compute_err)),
+                }
+                Err(parser_err) => Err(EvalError::ParserError(parser_err)),
+            }
+            Err(lexer_err) => Err(EvalError::LexerError(lexer_err)),
+        }
+    }
+
+    pub fn compute(&mut self, expr: &Expr) -> Result<f64, ComputeError> {
+        match expr {
+            Expr::Constant(num) => Ok(*num),
+            Expr::Identifier(id) => Err(ComputeError::UnrecognizedIdentifier(id.clone())),
+            Expr::Neg(expr) => Ok(-self.compute(expr)?),
+            Expr::BinOp(op, lexpr, rexpr) => {
+                let lnum = self.compute(&lexpr)?;
+                let rnum = self.compute(&rexpr)?;
+
+                match op {
+                    Operator::Plus => Ok(lnum + rnum),
+                    Operator::Minus => Ok(lnum - rnum),
+                    Operator::Star => Ok(lnum * rnum),
+                    Operator::Slash => Ok(lnum / rnum),
+                    Operator::Percent => Ok(lnum % rnum),
+                    _ => unimplemented!(),
+                }
+            }
+            Expr::Function(function, expr) => {
+                let num = self.compute(&expr)?;
+                Ok(match function {
+                    Function::Sqrt => num.sqrt(),
+                    Function::Sin => num.sin(),
+                    Function::Cos => num.cos(),
+                    Function::Tan => num.tan(),
+                    Function::Log => num.log10(),
+                    Function::Abs => num.abs(),
+                })
+            }
+            Expr::Assignment(id, expr) => {
+                let value = self.compute(&expr)?;
+                self.variables.insert(id.clone(), value);
+                Ok(value)
+            }
+            Expr::Pow(lexpr, rexpr) => {
+                Ok(self.compute(&lexpr)?.powf(self.compute(&rexpr)?))
+            }
+        }
+    }
+
+    pub fn get(&self, identifier: &str) -> Option<&f64> {
+        self.variables.get(identifier)
+    }
 }
 
 /// Turn an AST / Expr into an f64.
