7e9e215d4d4ad8d6d6850136624d9e61bc9c9a5c
Luke Wilson <asmoaesl@gmail.com>
2019-04-04 17:53:34 -0500

    Update rsc_benchmark.rs

--- benches/rsc_benchmark.rs
+++ benches/rsc_benchmark.rs
@@ -1,11 +1,34 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion, Benchmark};
 
-use rsc::eval;
+use rsc::{
+    eval,
+    lexer::tokenize,
+    parser::parse,
+    computer::Computer,
+};
 
 const INPUT: &'static str = "sqrt((6.1--2.22)^2 + (-24-10.5)^2)";
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("eval", |b| b.iter(|| eval(INPUT, std::f64::consts::PI, std::f64::consts::E)));
+    let tokens = tokenize(INPUT, true).unwrap();
+    let ast = parse(&tokens).unwrap();
+    let mut computer = Computer::new(std::f64::consts::PI, std::f64::consts::E);
+
+    // Tokenization
+    c.bench_function("lex", |b| b.iter(|| tokenize::<f64>(INPUT, true)));
+
+    // Parsing
+    c.bench_function("parse", move |b| b.iter(|| parse::<f64>(&tokens)));
+
+    // Computing
+    c.bench_function("compute", move |b| b.iter(|| computer.compute(&ast)));
+
+    // All at once
+    c.bench(
+        "eval",
+        Benchmark::new("indirectly", |b| b.iter(|| eval(INPUT, std::f64::consts::PI, std::f64::consts::E)))
+        .with_function("directly", |b| b.iter(|| Computer::new(std::f64::consts::PI, std::f64::consts::E).eval(INPUT)))
+    );
 }
 
 criterion_group!(benches, criterion_benchmark);
