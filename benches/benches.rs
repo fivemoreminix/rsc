abe5a77879e62d5f7e2fdafbb9c3eea34f68384a
Luke Wilson <thelukaswils@gmail.com>
2021-08-29 12:19:59 -0500

    Update benchmarks

--- benches/benches.rs
+++ benches/benches.rs
@@ -3,18 +3,38 @@
 extern crate test;
 
 use test::{Bencher, black_box};
-use rsc::tokenize;
+use rsc::{tokenize, parse};
 
-#[bench]
-fn bench_tokenizer_short_exprs(b: &mut Bencher) {
-    b.iter(|| {
-        tokenize(black_box("5.324 * 54(pad)"))
-    });
+const SHORT_STR: &str = "5.324 * 54(pad)";
+const LONG_STR: &str = "0.999998543 / sqrt(54 ^ (x(3)) % applesauce + bees";
+const FUNCTIONS_VARS: &str = "abs(5) + x(3) + abs(x(2)) + sqrt(4)";
+
+macro_rules! tokenizer_bench {
+    ($name:ident, $input:expr) => {
+        #[bench]
+        fn $name(b: &mut Bencher) {
+            b.iter(|| {
+                tokenize(black_box($input))
+            });
+        }
+    };
 }
 
-#[bench]
-fn bench_tokenizer_long_exprs(b: &mut Bencher) {
-    b.iter(|| {
-        tokenize(black_box("0.999998543 / sqrt(54 ^ (x(3)) % applesauce + bees"))
-    });
+macro_rules! parser_bench {
+    ($name:ident, $input:expr) => {
+        #[bench]
+        fn $name(b: &mut Bencher) {
+            let tokens = tokenize($input).unwrap();
+            b.iter(|| {
+                parse(black_box(&tokens))
+            })
+        }
+    };
 }
+
+tokenizer_bench!(tokenizer_short_expr, SHORT_STR);
+tokenizer_bench!(tokenizer_long_expr, LONG_STR);
+
+parser_bench!(parser_short_expr, SHORT_STR);
+parser_bench!(parser_long_expr, LONG_STR);
+parser_bench!(parser_function_vars, FUNCTIONS_VARS);
