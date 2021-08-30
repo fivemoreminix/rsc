38b7e8f657b1353fe725f3b79da55bd5c968e9be
Luke Wilson <thelukaswils@gmail.com>
2021-08-30 15:28:34 -0500

    Added zero() one() is_whole() and factorial() to Num trait

--- src/lib.rs
+++ src/lib.rs
@@ -10,20 +10,56 @@ pub use interpreter::*;
 
 use std::str::FromStr;
 use std::fmt::Debug;
-use std::ops::{Add, Mul, Sub, Div, Neg, Rem};
+use std::ops::{Add, Mul, Sub, Div, Neg, Rem, AddAssign, MulAssign, SubAssign, DivAssign};
 
-pub trait Num: Debug + Clone + PartialEq + FromStr + Add<Output=Self> + Sub<Output=Self> + Mul<Output=Self> + Div<Output=Self> + Rem<Output=Self> + Neg<Output=Self> {
-    fn pow(self, other: Self) -> Self;
+pub trait Num: Debug + Clone + PartialEq + PartialOrd + FromStr + Add<Output=Self> + Sub<Output=Self> + Mul<Output=Self> + Div<Output=Self> + Rem<Output=Self> + Neg<Output=Self> + AddAssign + SubAssign + MulAssign + DivAssign {
+    #[inline(always)] fn zero() -> Self;
+    #[inline(always)] fn one() -> Self;
+    #[inline] fn is_whole(&self) -> bool;
+    #[inline] fn pow(self, other: Self) -> Self;
+    fn factorial(self) -> Self {
+        let one = Num::one();
+        if self <= one {
+            one
+        } else {
+            let mut result = one;
+            let mut i = result.clone() + Num::one();
+            while i <= self {
+                result *= i.clone();
+                i += Num::one();
+            }
+            result
+        }
+    }
 }
 
 // Default impls for Num
 impl Num for f32 {
+    fn zero() -> Self {
+        0.0
+    }
+    fn one() -> Self {
+        1.0
+    }
+    fn is_whole(&self) -> bool {
+        self.fract() == 0.0
+    }
     #[inline(always)]
     fn pow(self, other: Self) -> Self {
         self.powf(other)
     }
 }
+
 impl Num for f64 {
+    fn zero() -> Self {
+        0.0
+    }
+    fn one() -> Self {
+        1.0
+    }
+    fn is_whole(&self) -> bool {
+        self.fract() == 0.0
+    }
     #[inline(always)]
     fn pow(self, other: Self) -> Self {
         self.powf(other)
