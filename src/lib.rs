5c00b313862a0ccfa957aeb9026665a7c63b5da7
Luke Wilson <thelukaswils@gmail.com>
2024-03-31 13:28:03 -0500

    Add documentation to Num trait

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@ use std::fmt::Debug;
 use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Rem, Sub, SubAssign};
 use std::str::FromStr;
 
+/// Defines the minimum operations and definitions to parse and evaluate expressions.
 pub trait Num:
     Debug
     + Clone
@@ -29,9 +30,13 @@ pub trait Num:
     + MulAssign
     + DivAssign
 {
+    /// Returns the additive identity value, 0, for the number.
     fn zero() -> Self;
+    /// Returns the multiplicative identity value, 1, for the number.
     fn one() -> Self;
+    /// Returns true if the number is a whole integer without a fractional part. E.g. 1 or 3.
     fn is_whole(&self) -> bool;
+    /// Returns number to the power of `other`.
     fn pow(self, other: Self) -> Self;
 }
 
